82b020e85034322b6ede2992863cf16bcfb9c68d
Harry_Yoon <haesoo9410@gmail.com>
2021-03-21 18:53:47 +0900

    Add time function in script

---
+++ script.js
@@ -0,0 +1,21 @@
+(() => {
+  // 시 분 반환 함수
+  let getTime = () => {
+    let date = new Date()
+    let hour = date.getHours().toString().padStart(2, '0');
+    let min = date.getMinutes().toString().padStart(2, '0');
+    
+    return (hour + " : " + min);
+  };
+  // 시 분 출력 함수
+  let showTime = () => {
+    let time = document.querySelector('.time');
+    time.innerHTML = getTime();
+
+    setTimeout(showTime(), 1000);
+  }
+
+  showTime()
+  
+
+})();
