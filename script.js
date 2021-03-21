bae9efe71b68b33a1fe9f777b2ee59e1762bca7a
Harry_Yoon <haesoo9410@gmail.com>
2021-03-21 19:14:47 +0900

    Fix time function

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
     let date = new Date()
     let hour = date.getHours().toString().padStart(2, '0');
     let min = date.getMinutes().toString().padStart(2, '0');
-    
+
     return (hour + " : " + min);
   };
   // 시 분 출력 함수
@@ -12,10 +12,10 @@
     let time = document.querySelector('.time');
     time.innerHTML = getTime();
 
-    setTimeout(showTime(), 1000);
+    setTimeout(showTime, 1000);
   }
-
-  showTime()
   
+  
+  showTime();
 
 })();
